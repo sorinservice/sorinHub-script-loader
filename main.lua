d353bd9b78b094edb7c075a9d7090b8705469a39
sorinservice <nemesisofnocturn@gmail.com>
2025-09-25 03:53:32 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 -- Orion laden
-local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/sorinservice/orion-lib/refs/heads/main/orion.lua"))()
+local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/sorinservice/script-libary/main/OrionLibary.lua"))()
 
 -- Fenster erstellen
 local Window = OrionLib:MakeWindow({
